66883b7ccb6384b3612238da42f377743fda5b8b
Zach <19974370+Zfauser@users.noreply.github.com>
2021-11-01 20:07:26 -0400

    Update and rename Hello.py to zfauser.py

--- Hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello, Github!")
-print("I'm @ZFauser")
-print("My code works because my code is open")
-print("Why did you execute this")
---
+++ zfauser.py
@@ -0,0 +1,8 @@
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+cls()
+name= input("What is your name? \n")
+print("Hello," name "i'm @Zfauser!")
+cls()
+
+
