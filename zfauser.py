84c106a192847a11d4344f1123eb684b8a0ac84e
Zach <19974370+Zfauser@users.noreply.github.com>
2021-11-01 20:27:14 -0400

    Update zfauser.py

--- zfauser.py
+++ zfauser.py
@@ -1,8 +1,11 @@
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
-cls()
+import time
 name= input("What is your name? \n")
-print("Hello," name "i'm @Zfauser!")
-cls()
+print("Hello,", name, "i'm @Zfauser!")
+time.sleep(1)
+print(name, "You're Very Cool")
+time.sleep(1)
+print("anyways gtg, Bye!!!!")
+time.sleep(1)
+print("(c) Zach Fauser 2021") 
 
 
